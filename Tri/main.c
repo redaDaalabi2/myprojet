eb083b8d36f4c1c788b78fc2ea653c1a2d0ef159
redaDaalabi2 <daalabir@gmail.com>
2021-11-15 16:19:23 +0100

    ad

--- Tri/main.c
+++ Tri/main.c
@@ -1,54 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void tri_selection(int *t, int n);
+#define Max 100
 
 int main()
 {
-    int *T, i, dim;
+    int T[Max], i, dim, min, posmin, DIM;
     //dimension du tableau
     do
     {
-      printf("Entrer la dimension du tableau\n");
+      printf("Entrer la dimension du tableau = ");
       scanf("%d",&dim);
-      if(dim<0)
-        dim * -1;
-    }while(dim==0);
-    T=(int *)malloc(dim * sizeof(int));
+    }while(dim>Max || dim<1);
+    DIM=dim;
     printf("remplissage de tableau\n");
     for(i=0;i<dim;i++)
     {
         printf("donner T[%d] = ",i+1);
         scanf("%d",&T[i]);
     }
-    printf("affichage du tableau avant le tri\n");
+    printf("affichage du tableau avant le tri par selection\n");
     for(i=0;i<dim;i++)
     {
-        printf("T[%d] = %d\n",i+1,T[i]);
+        printf(" %d\t",T[i]);
     }
-    tri_selection(T,dim);
-    printf("affichage du tableau apres le tri\n");
-    for(i=0;i<dim;i++)
+    while(dim>0)
     {
-        printf("T[%d] = %d\n",i+1,T[i]);
+        min=T[0];
+        posmin=0;
+        for(i=0;i<dim;i++)
+        {
+            if(T[i]>min)
+            {
+               min=T[i];
+               posmin=i;
+            }
+        }
+        for(i=posmin;i<dim;i++)
+        {
+            T[i]=T[i+1];
+        }
+        T[dim-1]=min;
+        dim--;
+    }
+    printf("\naffichage du tableau apres le tri\n");
+    for(i=0;i<DIM;i++)
+    {
+        printf(" %d\t",T[i]);
     }
     return 0;
 }
 
-void tri_selection(int *t, int n)
-{
-   int i, min, j , tmp;
-   for(i = 0 ; i < n - 1 ; i++)
-   {
-     min = i;
-     for(j = i+1 ; j < n ; j++)
-      if(t[j] < t[min])
-       min = j;
-        if(min != i)
-        {
-         tmp = t[i];
-         t[i] = t[min];
-         t[min] = tmp;
-        }
-   }
-}
